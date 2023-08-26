92a6929a3e0b19e9c0c38b20feb75bef7e206899
Zin Myo Win <mgzinmyowin12@gmail.com>
2023-08-26 19:40:40 +0630

    Added About tab

--- header.js
+++ header.js
@@ -70,3 +70,48 @@ window.onload = function() {
 };
 
 //Auto Text Writer Js End
+
+
+
+//Tabs Js Start
+
+function openPage(pageName, elmnt, backgroundColor, color,border) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i <tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    
+    // Remove the color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.color = "";
+    }
+    // Remove the border-color of all tablinks/buttons
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.borderColor= "";
+    }
+    
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor =  backgroundColor;
+    elmnt.style.color=color;
+    elmnt.style.borderColor=border;
+
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+  //---------------------------Tabs Js End----------------------
+  
